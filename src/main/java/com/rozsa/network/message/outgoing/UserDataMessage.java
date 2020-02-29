57f7803b1238f3c4c32e79964786dc27515a63a6
vitor.rozsa <vitor.rozsa@hotmail.com>
2020-02-29 18:31:55 -0300

    Add basic infra to send/receive user data

--- src/main/java/com/rozsa/network/message/outgoing/UserDataMessage.java
+++ src/main/java/com/rozsa/network/message/outgoing/UserDataMessage.java
@@ -6,4 +6,14 @@ public class UserDataMessage extends OutgoingMessage {
     public UserDataMessage(int size) {
         super(MessageType.USER_DATA, size);
     }
+
+    public void writeString(String value) {
+        byte[] valueAsBytes = value.getBytes();
+        if (dataIdx + valueAsBytes.length > value.length()) {
+            throw new ArrayIndexOutOfBoundsException(String.format("There is no enough buffer remaining to write %s", data));
+        }
+
+        System.arraycopy(valueAsBytes, 0, data, dataIdx, valueAsBytes.length);
+        dataIdx += valueAsBytes.length;
+    }
 }
